5dd419487101a677b505aa5cb9245b6a7186967e
AdelinChelariu <67071192+AdelinChelariu@users.noreply.github.com>
2021-04-11 19:09:58 +0300

    Add files via upload

---
+++ Practice/practiceH.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./practiceH.css">
+    <title>Practice HTML</title>
+</head>
+
+<body>
+    <span>
+
+
+        <a href="#" class="btn home">
+            Home
+        </a>
+
+        <a href="#" class="btn start">
+            Start
+        </a>
+
+        <a href="#" class="btn continue">
+            Continue
+        </a>
+
+    </span>
+</body>
+
+</html>
