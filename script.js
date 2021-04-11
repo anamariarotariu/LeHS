1a4275254916c7d19d3bc0357f79f1f76e514372
AdelinChelariu <adelinchelariu@gmail.com>
2021-04-11 19:10:33 +0300

    Practice

--- script.js
+++
@@ -1,7 +0,0 @@
-let registerBtn = document.getElementById('register_btn');
-let registerForm = document.querySelector('.register_form');
-let form = document.querySelector('.form');
-registerBtn.addEventListener('click', () =>{
-    form.classList.add('disappear');
-    registerForm.classList.remove('disappear');
-})
