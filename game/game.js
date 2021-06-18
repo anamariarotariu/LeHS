ea6773daa2d00913f4255027edd307128c4deecf
Ana-Maria Rotariu <anamariarotariu5@gmail.com>
2021-06-19 01:18:29 +0300

    Working quiz

--- game/game.js
+++ game/game.js
@@ -1,10 +1,10 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progress');
-const scoreHud = document.getElementById('score');
-const progressBarFull = document.getElementById('progress--bar--full');
-const loader = document.getElementById('loader');
-const game = document.getElementById('game');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progress");
+const scoreHud = document.getElementById("score");
+const progressBarFull = document.getElementById("progress--bar--full");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
 let acceptingAnswer = false;
 let score = 0;
@@ -12,86 +12,120 @@ let questionCounter = 0;
 let availableQuestion = [];
 
 let questions = [];
+function showHtmlQuestions() {
+  fetch("http://localhost:5000/api/html-questions")
+    .then((res) => {
+      return res.json();
+    })
+    .then((loadedQuestion) => {
+      questions = loadedQuestion.map((loadedQuestion) => {
+        const formattedQuestion = {
+          question: loadedQuestion.question,
+        };
+        const answerChoices = [
+          loadedQuestion.choice1,
+          loadedQuestion.choice2,
+          loadedQuestion.choice3,
+        ];
+        formattedQuestion.answer = Math.floor(Math.random() * 3 + 1);
+        answerChoices.splice(formattedQuestion.answer - 1, 0);
+        answerChoices.forEach((choice, index) => {
+          formattedQuestion["choice" + (index + 1)] = choice;
+        });
 
-fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple').then((res) =>{
-    return res.json();
-}).then(loadedQuestions =>{
-   questions = loadedQuestions.results.map((loadedQuestions) => {
-       const formattedQuestion = {
-           question: loadedQuestions.question
-       };
-       const answerChoices = [... loadedQuestions.incorrect_answers];
-       formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-       answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestions.correct_answer);
+        return formattedQuestion;
+      });
+      startGame();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function showCssQuestions() {
+  fetch("http://localhost:5000/api/css-questions/3")
+    .then((res) => {
+      return res.json();
+    })
+    .then((loadedQuestion) => {
+      questions = loadedQuestion.map((loadedQuestion) => {
+        const formattedQuestion = {
+          question: loadedQuestion.question,
+        };
+        const answerChoices = [
+          loadedQuestion.choice1,
+          loadedQuestion.choice2,
+          loadedQuestion.choice3,
+        ];
+        formattedQuestion.answer = Math.floor(Math.random() * 3 + 1);
+        answerChoices.splice(formattedQuestion.answer - 1, 0);
+        answerChoices.forEach((choice, index) => {
+          formattedQuestion["choice" + (index + 1)] = choice;
+        });
 
-       answerChoices.forEach((choice, index) =>{
-           formattedQuestion['choice' + (index+1)] = choice;
-       });
-       return formattedQuestion;
-   });
-   startGame();
-}).catch(err =>{
-    console.error(err);
-});
+        return formattedQuestion;
+      });
+      startGame();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 //Constants
 const CORRECT_BONUS = 20;
 const MAX_QUESTIONS = 5;
-
-startGame = () =>{
-    questionCounter = 0;
-    score = 0;
-    // spread operator => copy the content of the questions array inside the availableQuestion array (when we change one, the other changes as well)
-    availableQuestion = [...questions];
-    getNewQuestion();
-    game.classList.remove('hidden');
-    loader.classList.add('hidden');
-}
-getNewQuestion = () =>{
-    if(availableQuestion.length === 0 || questionCounter >= MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('end.php');
-    }
-    //populate the question container
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //update the progress bar
-    progressBarFull.style.width = `${questionCounter/MAX_QUESTIONS * 100}%`;
-  const questionIndex =  Math.floor(Math.random() * availableQuestion.length)
+showCssQuestions();
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestion = [...questions];
+  getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
+};
+getNewQuestion = () => {
+  if (availableQuestion.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  const questionIndex = Math.floor(Math.random() * availableQuestion.length);
   currentQuestion = availableQuestion[questionIndex];
   question.innerText = currentQuestion.question;
   //populate the answer fields
-  choices.forEach(choice => {
-      const number = choice.dataset['number'];
-      choice.innerText = currentQuestion['choice' + number];
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
   });
   availableQuestion.splice(questionIndex, 1);
   acceptingAnswer = true;
 };
-choices.forEach(choice => {choice.addEventListener('click', e => {
-   if(!acceptingAnswer) return;
-
-   acceptingAnswer = false;
-   const selectedChoice = e.target;
-   const selectedAnswer = selectedChoice.dataset['number'];
-
-   let classToApply = 'incorrect';
-   if (selectedAnswer == currentQuestion.answer){
-       classToApply = 'correct';
-   }
-  selectedChoice.parentElement.classList.add(classToApply);
-  if(classToApply === 'correct'){
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswer) {
+      return;
+    }
+    acceptingAnswer = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    let classToApply = "incorrect";
+    if (selectedAnswer == currentQuestion.answer) {
+      classToApply = "correct";
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
-  }
-  setTimeout( () =>{
-    selectedChoice.parentElement.classList.remove(classToApply);
-    getNewQuestion();
-  }, 1000);
-  
-   
-}); 
+    }
+    //for the user to see which is the correct answer
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
-incrementScore = (num) =>{
-    score += num;
-    scoreHud.innerText = score;
-}
+incrementScore = (num) => {
+  score += num;
+  scoreHud.innerText = score;
+};
--- game/game.php
+++ game/game.php
@@ -1,19 +1,20 @@
-<?php 
-  session_start(); 
+<?php
+session_start();
 
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: /Intermediate/login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: /Intermediate/login.php");
-  }
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: /Intermediate/login.php');
+}
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: /Intermediate/login.php");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="ro">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,73 +22,74 @@
     <link rel="stylesheet" href="game.css">
     <title>Game</title>
 </head>
+
 <body>
-     
-      <p> <a href="game.php?logout='1'">Logout</a> </p>
-   
+
+    <p> <a href="game.php?logout='1'">Logout</a> </p>
+
 
 
 
     <div class="container">
         <div id="loader"></div>
-            <div id="game" class="justify-center flex-column hidden">
-                
-                
-                <div id="hud">
-                    <div id="hud-item1">
-                        <p id="progress" class="hud-prefix">
-                        </p>
-                        <div id="progress--bar">
-                            <div id="progress--bar--full">
+        <div id="game" class="justify-center flex-column hidden">
 
-                            </div>
+
+            <div id="hud">
+                <div id="hud-item1">
+                    <p id="progress" class="hud-prefix">
+                    </p>
+                    <div id="progress--bar">
+                        <div id="progress--bar--full">
+
                         </div>
                     </div>
-                    <div id="hud-item2">
-                        <p class="hud-prefix">
-                            Score
-                        </p>
-                        <h1 class="hud-main-text" id="score"> 0</h1>
-                    </div>
                 </div>
-
-
-
-
-                <h2 id="question"> </h2>
-                <div class="choice-container">
-                    <p class="choice-prefix">A</p>
-                    <!-- data-number is a custom attribute  -->
-                    <p class="choice-text" data-number="1"></p>
-                    </div>
-                    <div class="choice-container">
-                        <p class="choice-prefix">B</p>
-                        <p class="choice-text" data-number="2"></p>
-                    </div>
-                    <div class="choice-container">
-                        <p class="choice-prefix">C</p>
-                        <p class="choice-text" data-number="3"></p>
-                    </div>
-                    
+                <div id="hud-item2">
+                    <p class="hud-prefix">
+                        Score
+                    </p>
+                    <h1 class="hud-main-text" id="score"> 0</h1>
                 </div>
+            </div>
 
 
 
-           <div class="flip-cover-hint"></div>  
-           
-           
-           <div class="btn">
+
+            <h2 id="question"> </h2>
+            <div class="choice-container">
+                <p class="choice-prefix">A</p>
+                <!-- data-number is a custom attribute  -->
+                <p class="choice-text" data-number="1"></p>
+            </div>
+            <div class="choice-container">
+                <p class="choice-prefix">B</p>
+                <p class="choice-text" data-number="2"></p>
+            </div>
+            <div class="choice-container">
+                <p class="choice-prefix">C</p>
+                <p class="choice-text" data-number="3"></p>
+            </div>
+
+        </div>
+
+
+
+        <div class="flip-cover-hint"></div>
+
+
+        <div class="btn">
             <div class="btn-back">
-              <p>Are you sure you want to do that?</p>
-              <a href="end.php" class="yes">Yes</a>
-              <button class="no">No</button>
+                <p>Are you sure you want to do that?</p>
+                <a href="end.php" class="yes">Yes</a>
+                <button class="no">No</button>
             </div>
             <div class="btn-front">Quit</div>
-          </div>
-            </div>
-<script src="game.js"></script>
-<script src="quitbutton.js"></script>
-<script src="hintbutton.js"></script>
+        </div>
+    </div>
+    <script src="game.js"></script>
+    <script src="quitbutton.js"></script>
+    <script src="hintbutton.js"></script>
 
 
 
@@ -95,4 +97,5 @@
 
 
 </body>
+
 </html>
